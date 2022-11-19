f03f33611fc6d5085c629e8e5488cf4310cc9b4e
Anna Logacheva <anna.s.litvinenko@gmail.com>
2022-11-19 14:37:01 +0600

    Implemented convert to card

--- src/components/ConvertToCurrencyCard.tsx
+++ src/components/ConvertToCurrencyCard.tsx
@@ -1,8 +1,36 @@
-import { View } from "react-native";
-import React, { FC } from "react";
+import { View, Text } from "react-native";
+import React, { FC, useEffect, useState } from "react";
+import { styles } from "../styles";
+import { TCurrency } from "../types";
+import Card from "../layout/Card";
+import { useData } from "../providers/DataProvider";
 
 const ConvertToCurrencyCard: FC = () => {
-  return <View></View>;
+  const { currentCurrency, totalAmount, currentFee } = useData();
+
+  const [convertToCurrency, setConvertToCurrency] = useState<TCurrency>("EUR");
+  const [convertToAmount, setConvertToAmount] = useState<number>(0);
+
+  useEffect(() => {
+    if (currentFee?.fxRate)
+      setConvertToAmount(Number((totalAmount * currentFee.fxRate).toFixed(2)));
+  }, [currentCurrency, totalAmount, currentFee]);
+
+  return (
+    <Card>
+      <View>
+        <Text style={styles.currencyText}>{convertToCurrency}</Text>
+        {currentFee?.fxRate && (
+          <Text
+            style={[styles.smallText, styles.violetText]}
+          >{`1 ${currentCurrency} =  ${currentFee.fxRate} ${convertToCurrency}`}</Text>
+        )}
+      </View>
+      <View>
+        <Text style={styles.bigAmountText}>{convertToAmount}</Text>
+      </View>
+    </Card>
+  );
 };
 
 export default ConvertToCurrencyCard;
